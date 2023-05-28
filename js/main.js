8c4c3f4c30740baf4a6703812b48c7a25a9edada
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-05-28 00:50:42 -0600

    update

--- js/main.js
+++ js/main.js
@@ -4,21 +4,37 @@ const tiposPokemon = document.getElementById('tiposPokemon');
 const pesoPokemon = document.getElementById('pesoPokemon');
 const imagenPokemon = document.getElementById('imagenPokemon');
 const alturaPokemon = document.getElementById('alturaPokemon');
-const movimientosPokemon = document.getElementById('movimientosPokemon');
-const estadisticasPokemon = document.getElementById('estadisticasPokemon');
+const divMovimientos = document.getElementById('divMovimientos');
+const divEstadisticas = document.getElementById('divEstadisticas');
 const idPokemon = document.getElementById('idPokemon');
+const pagina_pokemon_info = document.getElementById('pagina_pokemon_info');
 
-getPokemonPorId(
-  1
-); /**Esto es para obtener toda la informacion del pokemon que necesito
-El inicial siempre sera 1 */
+/**Objeto para asigna gradiente segun el tipo de pokemon */
+const coloresPokemon = {
+  normal: 'linear-gradient(to bottom, #D3D3D3, #FFFFFF)',
+  fighting: 'linear-gradient(to bottom, #A52A2A, #FF0000)',
+  flying: 'linear-gradient(to bottom, #87CEEB, #4169E1)',
+  poison: 'linear-gradient(to bottom, #800080, #EE82EE)',
+  ground: 'linear-gradient(to bottom, #D2B48C, #8B4513)',
+  rock: 'linear-gradient(to bottom, #A9A9A9, #696969)',
+  bug: 'linear-gradient(to bottom, #556B2F, #8FBC8F)',
+  ghost: 'linear-gradient(to bottom, #4B0082, #800080)',
+  steel: 'linear-gradient(to bottom, #708090, #A9A9A9)',
+  fire: 'linear-gradient(to bottom, #FF4500, #FFA500)',
+  water: 'linear-gradient(to bottom, #1E90FF, #00BFFF)',
+  grass: 'linear-gradient(to bottom, #006400, #32CD32)',
+  electric: 'linear-gradient(to bottom, #FFD700, #FFA500)',
+  psychic: 'linear-gradient(to bottom, #9932CC, #FF00FF)',
+  ice: 'linear-gradient(to bottom, #87CEEB, #00BFFF)',
+  dragon: 'linear-gradient(to bottom, #8A2BE2, #4B0082)',
+  dark: 'linear-gradient(to bottom, #2F4F4F, #000000)',
+  fairy: 'linear-gradient(to bottom, #FF69B4, #FF1493)',
+  unknown: 'linear-gradient(to bottom, #808080, #FFFFFF)',
+  shadow: 'linear-gradient(to bottom, #000000, #4B0082)',
+};
 
-/**
- * Esta funcion recibe como parametro el id y llama una funcion que retorna el
- * fetch realizado a la api para encadenarlo y recibir la respuesta
- * Luego se envia a la funcion mostrarPokemon
- * @param {idHtml} id
- */
+/**Con esta funcion se puede cambiar el id del pokemon que se desea obtener */
+getPokemonPorId(1);
 function getPokemonPorId(id) {
   apiObtenerPokemon(id).then((response) => {
     response.json().then((response) => {
@@ -27,102 +43,107 @@ function getPokemonPorId(id) {
     });
   });
 }
-/**
- *El fetch de la api se modifica segun el id del pokemon que se desee obtener
- se puede llamar en cualquier parte del codigo y retorna el fetch
- * @param {idHtml} id
- * @returns
+
+/**En esta funcion se convierte a string por si se pasa un dato de tipo number
+ * Esta retorna el fetch para obtener pokemon
  */
 function apiObtenerPokemon(id) {
   id = id.toString();
   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 }
 
-/**
- * La funcion de mostrar pokemon le asigna los valores de cada dato que necesitamos
- * de la respuesta de la api cada funcion obtiene el valor que necesita y todas
- * las funciones reciben como parametro la respuesta de la api
- * @param {JSON} pokemonApiResponse
- */
+/**Esta funcion asigna a los elementos html el valor de cada funcion en el codigo */
 function mostrarPokemon(pokemonApiResponse) {
   imagenPokemon.src = getImagenPokemon(pokemonApiResponse);
   nombrePokemon.innerHTML = getNombrePokemon(pokemonApiResponse);
   tiposPokemon.innerHTML = getTiposPokemon(pokemonApiResponse);
   pesoPokemon.innerText = getPesoPokemon(pokemonApiResponse);
   alturaPokemon.innerText = getAlturaPokemon(pokemonApiResponse);
-  movimientosPokemon.innerHTML = getMovimientosPokemon(pokemonApiResponse);
-  estadisticasPokemon.innerHTML = getEstadisticasPokemon(pokemonApiResponse);
+  mostrarMovimientos(pokemonApiResponse);
+  mostrarEstadisticas(pokemonApiResponse);
   idPokemon.innerHTML = getIdPokemon(pokemonApiResponse);
+  cambiaTema(pokemonApiResponse);
 }
-/**Recibimos la respuesta de la api y retornamos la respuesta para su id*/
 function getIdPokemon(pokemonApiResponse) {
   return pokemonApiResponse.id;
 }
-/**Retornamos el nombre */
 function getNombrePokemon(pokemonApiResponse) {
   return pokemonApiResponse.name;
 }
-/**Esta funcion primero toma el arreglo de objetos 'types'. Luego con la funcion
- * map() de los arreglos retornamos un arreglo con el nombre del tipo
- * tambien a una variable se le va concatenando un span con el valor del tipo de
- * pokemon al pokemon que recibimos de la api y la funcion retorna eso para asignarlo
- * en el html
- */
 function getTiposPokemon(pokemonApiResponse) {
   arrayTipos = pokemonApiResponse.types;
   let tiposSpans = '';
+  //Se recorre el array de tipos con map function y esto va creando elementos span
+  //Al final la funcion retorna la variable no el arreglo se podria hacer tambien con foreach
+  //aqui el map tambien sustituye el valor original del arreglo
   arrayTipos = arrayTipos.map(function (tipo) {
     tiposSpans += `<span>${tipo.type.name}</span>`;
     return tipo.type.name;
   });
-  console.log(arrayTipos);
   return tiposSpans;
 }
-/**Obtenemos la url de la imagen del pokemon */
+
 function getImagenPokemon(pokemonApiResponse) {
   return pokemonApiResponse.sprites.other['official-artwork'].front_default;
 }
-/**Retorna el peso */
+
 function getPesoPokemon(pokemonApiResponse) {
   return pokemonApiResponse.weight;
 }
-/**Retorna la altura */
 function getAlturaPokemon(pokemonApiResponse) {
   return pokemonApiResponse.height;
 }
-/**NAqui asignamos a una variable el array de objetos de tipo move
- * creamos la variable a agregar en la seccion del html y recorremos el arry
- * con la funcion map concatenamos el nombre del movimiento y retornamos la varible
- */
+/**Esto retorna el arrayMovimientos usando la funcion map */
 function getMovimientosPokemon(pokemonApiResponse) {
   arrayMovimientos = pokemonApiResponse.moves;
-  let movimientosSpan = '';
   arrayMovimientos = arrayMovimientos.map(function (movimiento) {
-    movimientosSpan += `<span>${movimiento.move.name}</span>`;
     return movimiento.move.name;
   });
-  return movimientosSpan;
+  return arrayMovimientos;
 }
 
-/**Obtenemos el array de la api response de las stats luego hacemos un primer map
- * este map es para crear un array con objetos estadistica con solo el valo de su
- * base stat y su nombre creamos un objeto por cada estadistica
- * Luego recorrecomos este arreglo y creamos una etiqueta span que se concatena
- * a la variable que retornamos al final de la funionc
+/**Mostramos los movimientos obtenidos con la funcion esta vez se utliza un foreacj
+ * y se limpia el contenido de adentro
  */
+function mostrarMovimientos(pokemonApiResponse) {
+  divMovimientos.innerHTML = '';
+  let arrayMovimientos = getMovimientosPokemon(pokemonApiResponse);
+  arrayMovimientos.forEach((movimiento) => {
+    let span = document.createElement('span');
+    span.append(movimiento);
+    divMovimientos.append(span);
+  });
+}
+/**En esta se usa el map para retornar la estadistica en un arreglo solo con lo que se necesita */
 function getEstadisticasPokemon(pokemonApiResponse) {
   arrayEstadisticas = pokemonApiResponse.stats;
   arrayEstadisticas = arrayEstadisticas.map((estadistica) => {
     return { valorBase: estadistica.base_stat, nombre: estadistica.stat.name };
   });
-  let estadisticasSpans = '';
+  return arrayEstadisticas;
+}
+/**Mostramos las estadisticas creando el elemento html pareciod a la funcion de mostrarMovimientos */
+function mostrarEstadisticas(pokemonApiResponse) {
+  divEstadisticas.innerHTML = '';
+  let arrayEstadisticas = getEstadisticasPokemon(pokemonApiResponse);
   arrayEstadisticas.forEach((estadistica) => {
-    estadisticasSpans += `<span>${estadistica.nombre}: ${estadistica.valorBase}</span>`;
+    let span = document.createElement('span');
+    span.append(`${estadistica.nombre}: ${estadistica.valorBase}`);
+    divEstadisticas.append(span);
   });
-  return estadisticasSpans;
+}
+/**Esto nos retorna el primer tipo de Pokemon */
+function getTipoPrincipal(pokemonApiResponse) {
+  let tipo = pokemonApiResponse.types[0].type.name;
+  return tipo;
+}
+/**Cambiamos el tipo de color en base al tipo retornado */
+function cambiaTema(pokemonApiResponse) {
+  let tipo = getTipoPrincipal(pokemonApiResponse);
+  console.log(tipo);
+  pagina_pokemon_info.style.background = coloresPokemon[tipo];
 }
 
-/**llama la funcion y parsea el id para hacer una suma y pasar al siguiente pokemon */
 const botonSiguiente = document.getElementById('botonSiguiente');
 botonSiguiente.addEventListener('click', function () {
   let siguientePokemon = parseInt(idPokemon.innerText);
@@ -130,7 +151,6 @@ botonSiguiente.addEventListener('click', function () {
   getPokemonPorId(siguientePokemon);
 });
 
-/**Parseamos y restamos 1 al id y luego pasamos al pokemon anterior */
 const botonAnterior = document.getElementById('botonAnterior');
 botonAnterior.addEventListener('click', function () {
   if (idPokemon.innerText <= 1) {
